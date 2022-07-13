3197c156d6c7a80ba40c1246db34397b55d431c0
SR <lsr_1105@ewhain.net>
2022-07-13 18:12:22 +0900

    feat: add test controller

---
+++ src/main/java/efub/team4/backend_eweather/domain/hello/controller/HelloController.java
@@ -0,0 +1,21 @@
+package efub.team4.backend_eweather.domain.hello.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+//@Api(tags = {"테스트 API"})
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping
+    //@ApiOperation(value = "테스트 조회", notes = "객체를 조회한다.")
+    public String hello() {
+        return "hello world!";
+    }
+
+}
