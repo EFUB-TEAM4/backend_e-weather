88ebb334528e9fd47b1dc2adb75bbddafe438db7
SR <lsr_1105@ewhain.net>
2022-07-13 17:46:04 +0900

    feat: add Spriing boot project for backend

---
+++ src/main/java/efub/team4/backend_eweather/BackendEWeatherApplication.java
@@ -0,0 +1,13 @@
+package efub.team4.backend_eweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEWeatherApplication.class, args);
+	}
+
+}
