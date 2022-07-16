5b8dd68471bb4326fe093c258bb51d5a8df83b4d
Sunny Lee <sunnyineverywhere@ewhain.net>
2022-07-17 06:45:42 +0900

    feature: get all users
    - 유저 정보 전체 조회
    
    #6

---
+++ src/main/java/efub/team4/backend_eweather/domain/user/dto/UserRequestDto.java
@@ -0,0 +1,12 @@
+package efub.team4.backend_eweather.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserRequestDto {
+
+
+}
