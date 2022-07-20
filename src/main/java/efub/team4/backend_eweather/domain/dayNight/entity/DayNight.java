fc89950f69f51a65b67b921af523978d87554461
SR <lsr_1105@ewhain.net>
2022-07-20 22:48:45 +0900

    fix: fix DayNight entity

--- src/main/java/efub/team4/backend_eweather/domain/dayNight/entity/DayNight.java
+++ src/main/java/efub/team4/backend_eweather/domain/dayNight/entity/DayNight.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Getter
@@ -24,12 +27,13 @@ public class DayNight {
     @NotEmpty
     private String timeName;
 
-    @Size(max = 50)
-    @NotEmpty
-    private String time;
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    @Column(columnDefinition = "TIME")
+    @NotNull
+    private LocalTime time;
 
     @Builder
-    public DayNight(String timeName, String time) {
+    public DayNight(String timeName, LocalTime time) {
         this.timeName = timeName;
         this.time = time;
     }
