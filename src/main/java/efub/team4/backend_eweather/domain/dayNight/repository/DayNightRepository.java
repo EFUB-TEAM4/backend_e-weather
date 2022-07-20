22df6b03fc89b27d3391eb675f572d0ffe209d69
SR <lsr_1105@ewhain.net>
2022-07-20 22:49:18 +0900

    feat: add DayNight repository

--- src/main/java/efub/team4/backend_eweather/domain/dayNight/repository/DayNightRepository.java
+++ src/main/java/efub/team4/backend_eweather/domain/dayNight/repository/DayNightRepository.java
@@ -3,9 +3,11 @@ package efub.team4.backend_eweather.domain.dayNight.repository;
 import efub.team4.backend_eweather.domain.dayNight.entity.DayNight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DayNightRepository extends JpaRepository<DayNight, UUID> {
-    Optional<DayNight> findByTime(String time);
+    Optional<DayNight> findByTime(LocalTime time);
+    Optional<DayNight> findByTimeName(String timeName);
 }
