b14b37c1b8ba65ce0cc6255bd67c7726d8fc9558
Sohyun-Dev <jf32430413@gmail.com>
2022-07-23 12:48:55 +0900

    feat : add temperature repository

--- src/main/java/efub/team4/backend_eweather/domain/temperature/repository/TemperatureRepository.java
+++ src/main/java/efub/team4/backend_eweather/domain/temperature/repository/TemperatureRepository.java
@@ -1,5 +1,6 @@
 package efub.team4.backend_eweather.domain.temperature.repository;
 
+import efub.team4.backend_eweather.domain.pty.entity.Pty;
 import efub.team4.backend_eweather.domain.temperature.entity.Temperature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,5 +8,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface TemperatureRepository extends JpaRepository<Temperature, UUID> {
-
+    Optional<Temperature> findByTemperature(Integer temperature);
 }
