146eaf9b1fc3146574d727f79d102c8ee02ee97e
Sohyun-Dev <jf32430413@gmail.com>
2022-07-23 20:41:08 +0900

    feat : add seasons entity

---
+++ src/main/java/efub/team4/backend_eweather/domain/seasons/entity/Seasons.java
@@ -0,0 +1,30 @@
+package efub.team4.backend_eweather.domain.seasons.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Seasons {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 16)
+    private UUID id;
+
+    @Size(max = 50)
+    @NotEmpty
+    private String seasonName;
+
+
+}
