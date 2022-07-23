c4302ca89e754bcbde7dbe168beb896fcfdd5b96
Sohyun-Dev <jf32430413@gmail.com>
2022-07-23 12:46:08 +0900

    feat : add bear service

---
+++ src/main/java/efub/team4/backend_eweather/domain/bear/service/BearService.java
@@ -0,0 +1,24 @@
+package efub.team4.backend_eweather.domain.bear.service;
+
+import efub.team4.backend_eweather.domain.bear.entity.Bear;
+import efub.team4.backend_eweather.domain.bear.exception.BearNotFoundException;
+import efub.team4.backend_eweather.domain.bear.repository.BearRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BearService {
+    private final BearRepository bearRepository;
+
+    @Transactional
+    public Bear save(Bear bear){
+        bearRepository.findById(bear.getId())
+                .ifPresent((existedBear) -> {
+                    throw new BearNotFoundException("Bear already exists with specified id = " + bear.getId());
+                });
+        return bearRepository.save(bear);
+    }
+
+}
