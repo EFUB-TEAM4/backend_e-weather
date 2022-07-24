45515269c48b70c91be3886daf2db306ced9bcda
SR <lsr_1105@ewhain.net>
2022-07-24 19:29:52 +0900

    fix: fix basetimeentity

--- src/main/java/efub/team4/backend_eweather/global/entity/BaseTimeEntity.java
+++ src/main/java/efub/team4/backend_eweather/global/entity/BaseTimeEntity.java
@@ -1,9 +1,9 @@
 package efub.team4.backend_eweather.global.entity;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,9 +16,23 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    @Column(name = "created_on")
     @CreatedDate
     private LocalDateTime createdOn;
 
+    @Column(name = "modified_on")
     @LastModifiedDate
     private LocalDateTime modifiedOn;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdOn = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        this.modifiedOn = this.createdOn;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.modifiedOn = LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+
 }
