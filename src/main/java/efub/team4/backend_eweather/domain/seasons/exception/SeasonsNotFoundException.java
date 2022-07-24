113dc48ad30a00a5ba115fd90cd8657bf494a501
Sohyun-Dev <jf32430413@gmail.com>
2022-07-24 17:32:47 +0900

    chore : delete seasons

--- src/main/java/efub/team4/backend_eweather/domain/seasons/exception/SeasonsNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package efub.team4.backend_eweather.domain.seasons.exception;
-
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-
-public class SeasonsNotFoundException extends ResourceNotFoundException {
-    public SeasonsNotFoundException(String message){
-        super(message);
-    }
-}
