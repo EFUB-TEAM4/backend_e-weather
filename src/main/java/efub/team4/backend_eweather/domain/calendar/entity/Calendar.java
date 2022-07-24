985ba5ea49e3a18aba9a0ab615c4d52c3be8337e
SR <lsr_1105@ewhain.net>
2022-07-24 19:28:30 +0900

    fix: fix calendar entity for uploading calendar

--- src/main/java/efub/team4/backend_eweather/domain/calendar/entity/Calendar.java
+++ src/main/java/efub/team4/backend_eweather/domain/calendar/entity/Calendar.java
@@ -2,6 +2,9 @@ package efub.team4.backend_eweather.domain.calendar.entity;
 
 import com.sun.istack.NotNull;
 
+import efub.team4.backend_eweather.domain.icon.entity.Icon;
+import efub.team4.backend_eweather.domain.pty.entity.Pty;
+import efub.team4.backend_eweather.domain.sky.entity.Sky;
 import efub.team4.backend_eweather.domain.user.entity.User;
 import efub.team4.backend_eweather.global.entity.BaseTimeEntity;
 import lombok.Builder;
@@ -21,9 +24,8 @@ public class Calendar extends BaseTimeEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(length = 16)
+    @Column(length = 16, name = "calendar_id")
     private UUID id;
-
 
     /**
      * 사용자
@@ -40,21 +42,44 @@ public class Calendar extends BaseTimeEntity {
     @NotNull
     private String description;
 
-
     @NotNull
-    private Integer temperature;
+    private String forecastDate;
 
-    @NotNull
-    private Integer max_temperature;
+    @Column(nullable = false)
+    private Integer minTemperature;
 
-    @NotNull
-    private Integer min_temperature;
+    @Column(nullable = false)
+    private Integer currentTemperature;
 
-    /**
-     * 강수 확률
-     */
+    @Column(nullable = false)
+    private Integer maxTemperature;
 
-    @Column(precision = 10, scale = 4)
-    private BigDecimal pty_probability;
+    @Column(nullable = false)
+    private Integer rainfallPercentage;
 
+    @OneToOne
+    @JoinColumn(name = "icon_id", nullable = false)
+    private Icon icon;
+
+    @OneToOne
+    @JoinColumn(name = "sky_id", nullable = false)
+    private Sky sky;
+
+    @OneToOne
+    @JoinColumn(name = "pty_id", nullable = false)
+    private Pty pty;
+
+    @Builder
+    public Calendar(User user, String description, String forecastDate, Integer minTemperature, Integer currentTemperature, Integer maxTemperature, Integer rainfallPercentage, Icon icon, Sky sky, Pty pty) {
+        this.user = user;
+        this.description = description;
+        this.forecastDate = forecastDate;
+        this.minTemperature = minTemperature;
+        this.currentTemperature = currentTemperature;
+        this.maxTemperature = maxTemperature;
+        this.rainfallPercentage = rainfallPercentage;
+        this.icon = icon;
+        this.sky = sky;
+        this.pty = pty;
+    }
 }
