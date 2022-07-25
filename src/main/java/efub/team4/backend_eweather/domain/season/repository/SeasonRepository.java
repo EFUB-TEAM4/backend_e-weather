5f2bf5bd24ac7a212a4901ebca403d74a57fe945
SR <lsr_1105@ewhain.net>
2022-07-26 06:36:07 +0900

    fix: fix loading season query for calculating in winter

--- src/main/java/efub/team4/backend_eweather/domain/season/repository/SeasonRepository.java
+++ src/main/java/efub/team4/backend_eweather/domain/season/repository/SeasonRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface SeasonRepository extends JpaRepository<Season, UUID> {
-    @Query(value = "SELECT * FROM SEASON WHERE START_MONTH <= ?1 AND ?1 <= END_MONTH", nativeQuery = true)
+    @Query(value = "SELECT * FROM SEASON WHERE CASE WHEN season_name='겨울' THEN START_MONTH <= ?1 OR ?1 <= END_MONTH\n" +
+            "    ELSE START_MONTH <= ?1 AND ?1 <= END_MONTH END", nativeQuery = true)
     Optional<Season> findByMonth(Integer month);
 
     Optional<Season> findSeasonBySeasonName(String name);
