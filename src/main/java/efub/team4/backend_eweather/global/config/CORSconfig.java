f7d73a10c7959d715a66d814a3f6c1563c3b97e6
SR <lsr_1105@ewhain.net>
2022-07-27 23:29:09 +0900

    fix: add Options metod in cors config

--- src/main/java/efub/team4/backend_eweather/global/config/CORSconfig.java
+++ src/main/java/efub/team4/backend_eweather/global/config/CORSconfig.java
@@ -10,7 +10,7 @@ public class CORSconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3030", "*")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
+                .allowedOrigins("http://localhost:3000", "*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS");
     }
 }
