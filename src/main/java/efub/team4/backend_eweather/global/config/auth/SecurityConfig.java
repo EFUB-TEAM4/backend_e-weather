e2ed4f51bb8af41af5f643b8ea19b565678ff7b6
SR <lsr_1105@ewhain.net>
2022-07-28 00:44:59 +0900

    fix: fix security config for http cors

--- src/main/java/efub/team4/backend_eweather/global/config/auth/SecurityConfig.java
+++ src/main/java/efub/team4/backend_eweather/global/config/auth/SecurityConfig.java
@@ -4,32 +4,94 @@ import efub.team4.backend_eweather.domain.user.service.CustomOauth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOauth2UserService customOauth2UserService;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/explorer/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v2/api-docs/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.cors()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().expressionHandler(expressionHandler())
+                .antMatchers(HttpMethod.GET, "/api/v1/eweather/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/bears/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/icons/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/calendars/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/votes/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/profiles/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/media/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/users/**").permitAll()
+                .antMatchers("/api/v1/media/**").permitAll()
+                .antMatchers("/api/v1/**")
+                .hasAnyRole("User")
+                .antMatchers("/",
+                        "/error",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers("/oauth2/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl("/")
+                .userInfoEndpoint()
+                .userService(customOauth2UserService);
+    }
+
+    /*
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .cors().and()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**", "/main", "/","/css/**","/images/**","/js/**","/profile").permitAll()
+                .antMatchers("/**", "/main", "/", "/css/**", "/images/**", "/js/**", "/profile").permitAll()
                 .antMatchers("api/v1/weather").permitAll()
                 .antMatchers("api/v1/bear").permitAll()
                 .antMatchers("api/v1/votes").hasAnyRole("USER")
@@ -45,9 +107,9 @@ public class SecurityConfig {
                 .userService(customOauth2UserService);
         return httpSecurity.build();
     }
-
+*/
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("*");
@@ -58,6 +120,27 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        Map<String, List<String>> roleHierarchyMap = new HashMap<>();
+
+        roleHierarchyMap.put("ROLE_ADMIN", Arrays.asList("ROLE_USER"));
+
+        String roles = RoleHierarchyUtils.roleHierarchyFromMap(roleHierarchyMap);
+        roleHierarchy.setHierarchy(roles);
+        return roleHierarchy;
+    }
+
+    @Bean
+    public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
+        DefaultWebSecurityExpressionHandler webSecurityExpressionHandler =
+                new DefaultWebSecurityExpressionHandler();
+        webSecurityExpressionHandler.setRoleHierarchy(roleHierarchy());
+        return webSecurityExpressionHandler;
     }
 
 }
