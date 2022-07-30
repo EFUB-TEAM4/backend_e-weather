35ad162b42b668e981c0f0717e3880787080e562
SR <lsr_1105@ewhain.net>
2022-07-30 16:49:35 +0900

    fix: add customer service to save current User

--- src/main/java/efub/team4/backend_eweather/global/outh/filter/TokenAuthenticationFilter.java
+++ src/main/java/efub/team4/backend_eweather/global/outh/filter/TokenAuthenticationFilter.java
@@ -1,12 +1,19 @@
 package efub.team4.backend_eweather.global.outh.filter;
 
+import efub.team4.backend_eweather.global.outh.service.CustomOAuth2UserService;
+import efub.team4.backend_eweather.global.outh.service.CustomUserDetailsService;
 import efub.team4.backend_eweather.global.outh.token.AuthToken;
 import efub.team4.backend_eweather.global.outh.token.AuthTokenProvider;
 import efub.team4.backend_eweather.global.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,27 +21,51 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final AuthTokenProvider tokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
-            FilterChain filterChain)  throws ServletException, IOException {
+            FilterChain filterChain) throws ServletException, IOException {
 
-        String tokenStr = HeaderUtil.getAccessToken(request);
+        String tokenStr = getJWTFromRequest(request);
+        System.out.println("token: " + tokenStr);
+
         AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+        System.out.println("claims: " + token.getTokenClaims());
 
-        if (token.validate()) {
-            Authentication authentication = tokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token.validate() && StringUtils.hasText(tokenStr)) {
+
+            String userId = token.getTokenClaims().get("sub", String.class);
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userId);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            //Authentication authentication = tokenProvider.getAuthentication(token);
+            //SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String getJWTFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
     }
 }
 
