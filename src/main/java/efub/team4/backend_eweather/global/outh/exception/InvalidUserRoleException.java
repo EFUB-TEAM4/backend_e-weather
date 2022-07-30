e47d861436cc52d7759affca9e2947774123ef4f
SR <lsr_1105@ewhain.net>
2022-07-30 19:08:08 +0900

    feat: add exception and handler for dealing with user role exception

---
+++ src/main/java/efub/team4/backend_eweather/global/outh/exception/InvalidUserRoleException.java
@@ -0,0 +1,7 @@
+package efub.team4.backend_eweather.global.outh.exception;
+
+public class InvalidUserRoleException extends RuntimeException{
+    public InvalidUserRoleException(String message){
+        super(message);
+    }
+}
