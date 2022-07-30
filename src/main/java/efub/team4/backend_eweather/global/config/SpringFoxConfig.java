56b5972c8847223b9a7e28b0deb4cdb7aacb5f4f
SR <lsr_1105@ewhain.net>
2022-07-30 16:50:13 +0900

    fix: fix swagger for adding header to authentication

--- src/main/java/efub/team4/backend_eweather/global/config/SpringFoxConfig.java
+++ src/main/java/efub/team4/backend_eweather/global/config/SpringFoxConfig.java
@@ -1,30 +1,29 @@
 package efub.team4.backend_eweather.global.config;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.AuthorizationScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableOpenApi
+//@EnableOpenApi
+@EnableSwagger2
 public class SpringFoxConfig {
     public static final Contact DEFAULT_CONTACT =
             new Contact(
@@ -46,10 +45,16 @@ public class SpringFoxConfig {
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
             new HashSet<String>(Arrays.asList("application/json", "application/xml"));
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
     @Bean
     public Docket api() {
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
@@ -57,6 +62,19 @@ public class SpringFoxConfig {
                 .apiInfo(DEFAULT_API_INFO)
                 .produces(DEFAULT_PRODUCES_AND_CONSUMES)
                 .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+
     }
 
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
