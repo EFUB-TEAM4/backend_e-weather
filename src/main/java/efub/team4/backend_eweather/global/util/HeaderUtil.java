1d8ae2d47fad64063901f3eb209f2aad54bca836
SR <lsr_1105@ewhain.net>
2022-07-30 19:06:54 +0900

    fix: fix headerUtil for dealing with bearer token

--- src/main/java/efub/team4/backend_eweather/global/util/HeaderUtil.java
+++ src/main/java/efub/team4/backend_eweather/global/util/HeaderUtil.java
@@ -1,6 +1,7 @@
 package efub.team4.backend_eweather.global.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 public class HeaderUtil {
 
     private final static String HEADER_AUTHORIZATION = "Authorization";
-    private final static String TOKEN_PREFIX = "Bearer ";
+    private final static String TOKEN_PREFIX = "Bearer";
 
     public static String getAccessToken(HttpServletRequest request) {
         String headerValue = request.getHeader(HEADER_AUTHORIZATION);
@@ -17,8 +18,8 @@ public class HeaderUtil {
             return null;
         }
 
-        if (headerValue.startsWith(TOKEN_PREFIX)) {
-            return headerValue.substring(TOKEN_PREFIX.length());
+        if (StringUtils.hasText(headerValue) && headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(7, headerValue.length());
         }
 
         return null;
