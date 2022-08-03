48148111784daf9ccc4455a394c80310af66f044
SR <lsr_1105@ewhain.net>
2022-08-03 17:26:49 +0900

    fix: fix config about cookies for frontend

--- src/main/java/efub/team4/backend_eweather/global/config/WebConfig.java
+++ src/main/java/efub/team4/backend_eweather/global/config/WebConfig.java
@@ -1,22 +1,27 @@
 package efub.team4.backend_eweather.global.config;
 
-import efub.team4.backend_eweather.global.config.auth.LogInUserArgumentResolver;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
-@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final LogInUserArgumentResolver resolver;
-
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
-        argumentResolvers.add(resolver);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations("file:/var/www/media/");
     }
 
+    /**
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(corsProperties.getAllowedOrigins())
+                .allowedMethods(corsProperties.getAllowedMethods())
+                .allowedHeaders(corsProperties.getAllowedHeaders())
+                .allowCredentials(true)
+                .maxAge(corsProperties.getMaxAge());
+    }**/
 }
