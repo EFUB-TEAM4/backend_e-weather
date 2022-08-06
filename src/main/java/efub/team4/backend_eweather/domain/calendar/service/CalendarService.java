7e5aa0962e94c21c33e5943130952f65605d7c6f
SR <lsr_1105@ewhain.net>
2022-08-07 04:02:16 +0900

    fix: hotfix for handling calendar exception

--- src/main/java/efub/team4/backend_eweather/domain/calendar/service/CalendarService.java
+++ src/main/java/efub/team4/backend_eweather/domain/calendar/service/CalendarService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -22,8 +23,10 @@ public class CalendarService {
 
     @Transactional
     public Calendar save(Calendar calendar) {
-        calendarRepository.findCalendarByForecastDate(calendar.getForecastDate())
-                .ifPresent((existedCalendar) -> new CalendarAlreadyExistsException("Calendar is already exists with date=" + calendar.getForecastDate()));
+        Optional<Calendar> existedCalendar = calendarRepository.findCalendarByForecastDateAndUser_Id(calendar.getForecastDate(), calendar.getUser().getId());
+        if (existedCalendar.isPresent()){
+            throw new CalendarAlreadyExistsException("Calendar is already exists with date=" + calendar.getForecastDate());
+        }
         return calendarRepository.save(calendar);
     }
 
