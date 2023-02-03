72db79992950167097111a96076fc90c5f01319b
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-02-03 23:32:13 +0900

    chore: replace yml to properties
    - 배포 편리를 위해 교체

--- src/main/java/efub/team4/backend_eweather/global/config/properties/CorsProperties.java
+++ src/main/java/efub/team4/backend_eweather/global/config/properties/CorsProperties.java
@@ -3,6 +3,7 @@ package efub.team4.backend_eweather.global.config.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @Getter
 @Setter
--- src/main/java/efub/team4/backend_eweather/global/config/security/JwtConfig.java
+++ src/main/java/efub/team4/backend_eweather/global/config/security/JwtConfig.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:/application.properties")
 public class JwtConfig {
-    @Value("${jwt.secret}")
+    @Value("${spring.jwt.secret}")
     private String secret;
 
     @Bean
